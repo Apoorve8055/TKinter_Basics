d7a54301762690fc84e8c80040a70346426c53bb
Apoorv <vapoorve@gmail.com>
2019-06-24 12:08:12 +0530

    All about labels

--- Layout01/part03_Label.py
+++ Layout01/part03_Label.py
@@ -2,7 +2,7 @@ from tkinter import *
 root = Tk()
 root.title("Apoorv Verma")
 root.geometry("720x420")
-Label(text="ALL RIGHTS RESERVED" , bg="grey" , fg="white" , padx=720 , pady=5 , font="Arial 12 bold",borderwidth=3, relief=GROOVE).pack(side=BOTTOM)
+Label(text="ALL RIGHTS RESERVED" , bg="grey" , fg="white" , padx=720 , pady=5 , font="Arial 12 bold",borderwidth=3, relief=GROOVE).pack(side=BOTTOM,fill=X)
 Label(text="Ready To Go", fg="black" , padx=20 , pady=5 , font="Arial 6 italic",borderwidth=3, relief=RIDGE).pack(anchor="sw", side=TOP)
 root.mainloop()
 
